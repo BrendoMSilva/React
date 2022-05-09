702da93b20307abecfad4e11457ddadc118ebca9
BrendoSilva <80553851+BrendoMSilva@users.noreply.github.com>
2022-05-08 21:50:04 -0300

    hooks

--- 3_AVANCANDO_EM_REACT/avancando/src/App.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.js
@@ -1,20 +1,19 @@
 import './App.css';
-import City from "./assets/city.jpg";
+import City from "./assets/city.jpg"
+import ManageData from './components/ManageData';
 
 function App() {
   return (
     <div className="App">
-       <h1>Avançando em React</h1>
-       {/* Imagem em public */}
-       <div>
-         <img src="/img1.jpg" alt="Paisagem" />
-       </div>
-       {/* Imagem em public */}
-       <div>
-         <img src={City} alt="Cidade" />
-       </div>
+      <h1>Avançando em react</h1>
+      <div>
+        <img src="/img1.jpg" alt="paisagem" />
+      </div>
+      <div>
+        <img src= {City} alt="cidade" />
+      </div>
+      <ManageData/>
     </div>
   );
 }
-
 export default App;
